c0e085d0d839dfe5cfdf3787563c2238671cd167
AlanMartinezDev <alanmartiola@gmail.com>
2023-12-05 11:05:44 +0100

    update webp and minify img

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,39 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+// CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+
+// Imagenes
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const cache = require("gulp-cache");
 
 function css(done) {
   src("src/scss/**/*.scss") // Identificar el archivo SASS
     .pipe(plumber())
     .pipe(sass()) // Compilarlo
     .pipe(dest("build/css")); // Almacenarla en el disco duro
-
   done(); // Callback que avisa a Gulp cuando llegamos al final
+}
+
+function imagenes(done) {
+  const opciones = {
+    optimizationLevel: 3,
+  };
+  src("src/img/**/*.{png,jpg}")
+    .pipe(cache(imagemin(opciones)))
+    .pipe(dest("build/img"));
+  done();
+}
+
+function versionWebp(done) {
+  const opciones = {
+    quality: 50,
+  };
+
+  src("src/img/**/*.{png,jpg}").pipe(webp(opciones)).pipe(dest("build/img"));
+  done();
 }
 
 function dev(done) {
@@ -17,4 +42,6 @@ function dev(done) {
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(imagenes, versionWebp, dev);
