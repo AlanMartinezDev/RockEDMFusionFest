5b4b07b4a60818f78d49a2f175ea84ea2a667f31
AlanMartinezDev <alanmartiola@gmail.com>
2023-12-05 11:57:32 +0100

    añadir avif

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,10 @@ const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
 
 // Imagenes
+const cache = require("gulp-cache");
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
-const cache = require("gulp-cache");
+const avif = require("gulp-avif");
 
 function css(done) {
   src("src/scss/**/*.scss") // Identificar el archivo SASS
@@ -36,6 +37,15 @@ function versionWebp(done) {
   done();
 }
 
+function versionAvif(done) {
+  const opciones = {
+    quality: 50,
+  };
+
+  src("src/img/**/*.{png,jpg}").pipe(avif(opciones)).pipe(dest("build/img"));
+  done();
+}
+
 function dev(done) {
   watch("src/scss/**/*.scss", css);
   done();
@@ -44,4 +54,5 @@ function dev(done) {
 exports.css = css;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.dev = parallel(imagenes, versionWebp, dev);
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
